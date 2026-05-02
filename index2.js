14b6986b8918e0dddf12f436e33f8cbcbce4c00c
Khushi Agrawal <khushiagrawal5527@gmail.com>
2026-05-02 11:20:01 +0530

    Day2-Template Literals,Arrow Functions,Array Function

--- index2.js
+++ index2.js
@@ -28,7 +28,7 @@ function table(num) {
 let sum = (x, y) => x + y; // Arrow function for addition
 console.log("Sum of 5 and 10:", sum(5, 10)); // 15
 
-let arr = [1, 2, 3, 4, 5, "Aaryan"];
+let arr = [1, 2, 3, 4, 5, "Khushi"];
 
 // arr.filter(function(x) {
 //     return typeof x === "number";
@@ -39,5 +39,50 @@ let arr = [1, 2, 3, 4, 5, "Aaryan"];
 // });
 
 arr.filter(x => typeof x === "number").map(y => y * 2).forEach(abc => console.log("Element after * 2:", abc * 2));
-// console.log(arr.find(x => x === "Aaryan1")); 
-// Arrow function is represented by =>
+// console.log(arr.find(x => x === "shiv")); 
+
+
+console.log(Math.floor(Math.random() * 100) + 1); // 1 to 100
+
+
+let strnum = "5";
+console.log(Number(strnum));
+console.log(String(5));
+
+let val = "0.5";
+console.log(parseInt(val)); // 0
+console.log(parseFloat(val)); // 0.5
+
+// floor [int] -> Low int, parseInt -> type conversion (string -> int)
+let colors = ["red", "green", "blue"];
+console.log(colors.indexOf("yellow")); // -1
+console.log(colors.includes("yellow")); // false
+console.log(colors.length); // 3
+console.log(colors[3]); // undefined
+console.log(colors[colors.length - 1]); // "blue"
+colors.push("yellow"); // Adds "yellow" to the end of the array
+console.log(colors); // ["red", "green", "blue", "yellow"]
+colors.pop(); // Removes the last element ("yellow")
+console.log(colors); // ["red", "green", "blue"]
+colors.unshift("purple"); // Adds "purple" to the beginning of the array
+console.log(colors); // ["purple", "red", "green", "blue"]
+colors.shift(); // Removes the first element ("purple")
+console.log(colors); // ["red", "green", "blue"]
+// [red] [green] [blue] --> undefined
+
+// let indexOf = "yellow";
+// for (let i = 0; i < colors.length; i++) {
+//     if(colors[i] === indexOf) {
+//         return i;
+//     } if(i == colors.length - 1) {
+//         return -1;
+//     }
+// }
+
+// remove by index, obj, [1, length - 2] --> math.random() * (length - 2)
+// console.log(colors.splice(0, 1));
+// console.log(colors.splice(1, 2));
+// console.log(colors.splice(1, 3));
+// console.log(colors.splice(Math.floor(Math.random() * (colors.length - 2)), 1));
+// let str = "Hello World";
+// console.log(str.slice(0, 5)); // "Hello"
