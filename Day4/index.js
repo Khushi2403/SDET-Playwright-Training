55914122c0c91601aefe55df3c52477a6d4d615a
Khushi Agrawal <khushiagrawal5527@gmail.com>
2026-05-05 10:21:30 +0530

    promise

--- Day4/index.js
+++ Day4/index.js
@@ -1,16 +1,16 @@
-function getData() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("Data received");
-        }, 2000);
-    });
-}
+// function getData() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve("Data received");
+//         }, 2000);
+//     });
+// }
  
-getData().then((data) => {
-    console.log(data);
-}).catch((error) => {
-    console.error("Error:", error);
-});
+// getData().then((data) => {
+//     console.log(data);
+// }).catch((error) => {
+//     console.error("Error:", error);
+// });
 // setTimeout(() => {
 //     console.log("Data received");
 // }, 5000);
@@ -18,4 +18,24 @@ getData().then((data) => {
 // setInterval(() => {
 //     console.log("Checking for new data...");
 // }, 3000);
+
+
+
+//reject
+
+function getData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject("Failed to get data ");
+        }, 2000);
+    });
+}
+ 
+getData()
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => {
+    console.error("Error:", error);
+});
  
