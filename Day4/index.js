285b633cf07c4cc0fd44e39307412e6747d66850
Khushi Agrawal <khushiagrawal5527@gmail.com>
2026-05-05 12:22:42 +0530

    Promise,Fetching data from API

--- Day4/index.js
+++ Day4/index.js
@@ -23,19 +23,44 @@
 
 //reject
 
-function getData() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            reject("Failed to get data ");
-        }, 2000);
-    });
-}
+// function getData() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             reject("Failed to get data ");
+//         }, 2000);
+//     });
+// }
  
-getData()
-.then((data) => {
-    console.log(data);
-})
-.catch((error) => {
-    console.error("Error:", error);
-});
+// getData()
+// .then((data) => {
+//     console.log(data);
+// })
+// .catch((error) => {
+//     console.error("Error:", error);
+// });
+
+
+
+
+
+//  function getData() {
+//     return new Promise((resolve, reject) => {
+//         if(2 > 3) {
+ 
+//             setTimeout(() => {
+//                 resolve("Data received");
+//             }, 2000);
+//         } else {
+//             reject('not working')
+//         }
+//     });
+// }
+ 
+ // fetching data from api
+fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(res => res.json()) // res -> string -> json)
+    .then(data => data.filter(d => d.id % 2 === 0))
+    .then(data => data.map(d => (d.title = "Khushi " + d.title)))
+    .then(data => console.log(data))
+    .catch(error => console.error("Error fetching data:", error));
  
