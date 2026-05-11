8ae806bf221f8a2e982b19ddfd4462b92995481c
Khushi Agrawal <khushiagrawal5527@gmail.com>
2026-05-11 12:41:44 +0530

    typescript

--- Day9/index.ts
+++ Day9/index.ts
@@ -1,2 +1,12 @@
-let name="khushi";
-console.log(name);
+let name: string = "khushi";
+let result: number | string;
+result = "success";
+result = 45;
+
+console.log(name, result);
+ 
+function add(a: number, b: number): string {
+    return "Sum: " + (a + b);
+}
+ 
+console.log(add(5, 10));
