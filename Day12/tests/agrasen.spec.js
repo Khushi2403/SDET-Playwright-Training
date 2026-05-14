305af20e2c641e207a9305e039e77e241603d415
Khushi Agrawal <khushiagrawal5527@gmail.com>
2026-05-14 14:26:15 +0530

    Playwright basics

---
+++ Day12/tests/agrasen.spec.js
@@ -0,0 +1,19 @@
+import{test,expect} from '@playwright/test';
+
+// test('has title',async({page})=>{
+//     await page.goto("https://www.indiamart.com/shree-agrasen-agro-industries/");
+
+//     await expect(page).toHaveTitle(/Agrasen Agro/);
+
+// });
+
+
+
+// test('click Contact Us and verify Reach Us section', async ({ page }) => {
+//   await page.goto('https://www.indiamart.com/shree-agrasen-agro-industries/');
+
+//   await page.getByRole('link', { name: 'Contact Us' }).first().click();
+
+//   await expect(page.getByText('Reach Us')).toBeVisible();
+// });
+
